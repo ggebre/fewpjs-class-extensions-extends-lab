7fd4b718e53b6bfb3dd516757db1a31add01f5a0
Getu Gebre <getu.gebre.gg@gmail.com>
2021-01-21 22:22:21 -0600

    done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+class Polygon {
+    constructor (array){
+        this.array = array
+    }
+    get countSides() {
+        return this.array.length
+    } 
+    get perimeter() {
+        return this.array.reduce((memo, e) => memo + e, 0)
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() { 
+
+        return this.countSides == 3 && !this.array.some(elem => this.perimeter - elem < elem )
+    }
+} 
+class Square extends Polygon {
+    get isValid() {
+        
+        return this.countSides == 4 && !this.array.some(elem => elem != this.array[0])
+    } 
+    get area(){
+        if (this.isValid){
+            return this.array[0]**2 
+        }
+    }
+}
